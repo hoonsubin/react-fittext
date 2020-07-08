7471b9f3bc6e18324201d1505bd779fbc107469b
Hoon Kim <hoonsubin@icloud.com>
2020-07-08 08:43:01 -0700

    fixed tests and package list

--- src/components/__tests__/FitText.spec.tsx
+++ src/components/__tests__/FitText.spec.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { FitText } from '../../index';
+import FitText from '../../index';
+import TestRenderer from 'react-test-renderer';
 
-test('Renders', async () => {
-    const { getByRole } = render(<FitText />);
-    expect(getByRole('heading')).toBeInTheDOM();
+test('Renders component', async () => {
+    const testRenderer = TestRenderer.create(
+        <FitText>
+            <p className="text">Hello Test</p>
+        </FitText>,
+    );
+
+    expect(testRenderer.toJSON()?.type).toBe('div');
 });
